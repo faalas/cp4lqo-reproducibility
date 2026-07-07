08b9fff1bc356fa611a5d7944dd60db75c2e7de0
Faisal Azmi Sirajuddin <162958780+faalas@users.noreply.github.com>
2026-07-07 23:03:49 +0700

    upload folders (conf, load-postgres, pg_executor)

--- Balsa/load-postgres/load_job_postgres.sh
+++ Balsa/load-postgres/load_job_postgres.sh
@@ -18,6 +18,7 @@ set -ex
 DATA_DIR=$1
 DBNAME=${2:-imdbload}
 
+dropdb --if-exists $DBNAME
 createdb $DBNAME
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -25,27 +26,27 @@ psql $DBNAME -f "$DIR/schema.sql"
 psql $DBNAME -f "$DIR/fkindexes.sql"
 
 pushd $DATA_DIR
-psql $DBNAME -c "\copy name from '$1/name.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy char_name from '$1/char_name.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy comp_cast_type from '$1/comp_cast_type.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy company_name from '$1/company_name.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy company_type from '$1/company_type.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy info_type from '$1/info_type.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy keyword from '$1/keyword.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy kind_type from '$1/kind_type.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy link_type from '$1/link_type.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy role_type from '$1/role_type.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy title from '$1/title.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy aka_name from '$1/aka_name.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy aka_title from '$1/aka_title.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy cast_info from '$1/cast_info.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy complete_cast from '$1/complete_cast.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy movie_companies from '$1/movie_companies.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy movie_info from '$1/movie_info.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy movie_info_idx from '$1/movie_info_idx.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy movie_keyword from '$1/movie_keyword.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy movie_link from '$1/movie_link.csv' escape '\' csv header" &
-psql $DBNAME -c "\copy person_info from '$1/person_info.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy name from 'name.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy char_name from 'char_name.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy comp_cast_type from 'comp_cast_type.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy company_name from 'company_name.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy company_type from 'company_type.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy info_type from 'info_type.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy keyword from 'keyword.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy kind_type from 'kind_type.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy link_type from 'link_type.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy role_type from 'role_type.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy title from 'title.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy aka_name from 'aka_name.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy aka_title from 'aka_title.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy cast_info from 'cast_info.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy complete_cast from 'complete_cast.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy movie_companies from 'movie_companies.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy movie_info from 'movie_info.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy movie_info_idx from 'movie_info_idx.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy movie_keyword from 'movie_keyword.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy movie_link from 'movie_link.csv' escape '\' csv header" &
+psql $DBNAME -c "\copy person_info from 'person_info.csv' escape '\' csv header" &
 wait
 popd
 
